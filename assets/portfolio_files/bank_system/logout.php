76608e58a236cfabea1d4b27b269dc2900a10bd6
Naalvino <naalvino@optonline.net>
2020-04-16 19:33:40 -0400

    SEO Fixes for main site
    Optimization for nicholasalvino.com

--- assets/portfolio_files/bank_system/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-setcookie(login_successful,$htmllogin,time()-3600);
-echo "<HTML>";
-echo "You successfully sign out of your account!";
-echo "<br><a href='proj2.html'>Return to Home Page</a>";
-?>
