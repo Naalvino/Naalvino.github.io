87800f30277b8e3c31bcf29b059309af73128286
Nicholas Alvino <44778702+Naalvino@users.noreply.github.com>
2020-04-16 19:17:52 -0400

    Delete contact_form_script.js

--- assets/js/contact_form_script.js
+++
@@ -1,8 +0,0 @@
-<script language="JavaScript">
-    var formvalidator = new Validator(contactform);
-    formvalidator.addValidation("name","req","Please provide your name");
-    formvalidator.addValidation("email","req","Please provide your email");
-    formvalidator.addValidation("email","email","Please enter a valid email address");
-</script>
-
-
